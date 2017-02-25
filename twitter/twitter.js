6a4b0d742f295e9bb762d7197793cf03dd04cd7a
mailsvb <mailsvb@gmail.com>
2017-02-25 23:35:44 +0100

    1.3.0

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -35,126 +35,6 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("twitter-api-connection",TwitterAPIConnection);
-
-    //Twitter stream of users
-    function TwitterStreamUser(n) {
-        RED.nodes.createNode(this,n);
-        var node = this;
-        node.follow = n.follow;
-        node.connection = RED.nodes.getNode(n.connection);
-        node.status({fill:"yellow",shape:"dot",text:"connecting"});
-        
-        node.connection.client.get('users/lookup', {screen_name: node.follow}, function(error, data, response){
-            if (error) {
-                node.status({fill:"red",shape:"dot",text:"error"});
-                node.error(util.inspect(error, { showHidden: true, depth: null }));
-                return;
-            }
-            try {
-                var all_IDs = [];
-                var all_names = [];
-                for (var i=0; i < data.length; i++)
-                {
-                    all_IDs.push(data[i].id_str.toString());
-                    all_names.push(data[i].name.toString());
-                }
-                node.log('streaming IDs: ' + all_IDs.join(', ').toString());
-                
-                node.stream = node.connection.client.stream('statuses/filter', { follow: all_IDs.join(',').toString() });
-                
-                node.stream.on('connect', function (request) {
-                    node.log('streaming API connecting');
-                    node.status({fill:"yellow",shape:"dot",text:"connecting"});
-                });
-                
-                node.stream.on('connected', function (response) {
-                    node.log('streaming API connected. Streaming names: ' + all_names.join(', ').toString());
-                    node.status({fill:"green",shape:"dot",text:"connected"});
-                });
-                
-                node.stream.on('disconnect', function (disconnectMessage) {
-                    node.log('streaming API disconnected ' + util.inspect(disconnectMessage, { showHidden: true, depth: null }));
-                    node.status({fill:"red",shape:"dot",text:"disconnected"});
-                });
-                
-                node.stream.on('reconnect', function (request, response, connectInterval) {
-                    node.log('streaming API reconnecting');
-                    node.status({fill:"yellow",shape:"dot",text:"reconnecting"});
-                });
-                    
-                node.stream.on('tweet', function(tweet) {
-                    if (all_IDs.indexOf(tweet.user.id_str) > -1)
-                    {
-                        node.log(tweet.user.name + ': ' + tweet.text);
-                        var msg = {
-                            payload: tweet
-                        };
-                        node.send(msg);
-                    }
-                });
-            }
-            catch (error) {
-                node.error(util.inspect(error, { showHidden: true, depth: null }));
-            }
-        });
-        
-        this.on("close", function() {
-            if (node.stream) {
-                node.log('stopping stream on close');
-                node.stream.stop();
-                delete node.stream;
-            }
-        });
-    }
-    RED.nodes.registerType("Twitter Users",TwitterStreamUser);
-    
-        //Twitter stream of topics
-    function TwitterStreamTopics(n) {
-        RED.nodes.createNode(this,n);
-        var node = this;
-        node.topics = n.topics;
-        node.connection = RED.nodes.getNode(n.connection);
-        node.status({fill:"yellow",shape:"dot",text:"connecting"});
-        
-        node.stream = node.connection.client.stream('statuses/filter', {track: node.topics});
-        
-        node.stream.on('connect', function (request) {
-            node.log('streaming API connecting');
-            node.status({fill:"yellow",shape:"dot",text:"connecting"});
-        });
-        
-        node.stream.on('connected', function (response) {
-            node.log('streaming topics: ' + node.topics);
-            node.status({fill:"green",shape:"dot",text:"connected"});
-        });
-        
-        node.stream.on('disconnect', function (disconnectMessage) {
-            node.log('streaming API disconnected ' + util.inspect(disconnectMessage, { showHidden: true, depth: null }));
-            node.status({fill:"red",shape:"dot",text:"disconnected"});
-        });
-        
-        node.stream.on('reconnect', function (request, response, connectInterval) {
-            node.log('streaming API reconnecting');
-            node.status({fill:"yellow",shape:"dot",text:"reconnecting"});
-        });
-            
-        node.stream.on('tweet', function(tweet) {
-            node.log(tweet.user.name + ': ' + tweet.text);
-            var msg = {
-                payload: tweet
-            };
-            node.send(msg);
-        });
-        
-        this.on("close", function() {
-            if (node.stream) {
-                node.log('stopping stream on close');
-                node.stream.stop();
-                delete node.stream;
-            }
-        });
-    }
-    RED.nodes.registerType("Twitter Topics",TwitterStreamTopics);
     
     //Twitter stream
     function TwitterStream(n) {
@@ -162,7 +42,9 @@ module.exports = function(RED) {
         var node = this;
         node.follow = n.follow || "";
         node.topics = n.topics || "";
+        node.tweetLimit = parseInt(n.tweetLimit) || 10;
         node.topicRetweets = n.topicRetweets || false;
+        node.alwaysVerified = n.alwaysVerified || false;
         node.topicLanguage = n.topicLanguage.toString().trim().split(",") || ['en','de'];
         node.loadImages = n.loadImages || false;
         node.debug = n.debug || false;
@@ -170,6 +52,7 @@ module.exports = function(RED) {
         node.userNames = [];
         node.userIDs = [];
         node.streamOptions = {};
+        node.tweetLimitCount = 0;
         node.connection = RED.nodes.getNode(n.connection);
         node.status({fill:"yellow",shape:"dot",text:"connecting"});
         
@@ -229,16 +112,32 @@ module.exports = function(RED) {
                     (node.debug === true) ? node.log(util.inspect(tweet, { showHidden: true, depth: null })) : node.log(tweet.user.name + ': ' + tweet.text);
                     
                     if (node.topicRetweets === false && node.userIDs.indexOf(tweet.user.id_str) < 0 && tweet.retweeted_status) {
-                        node.log('skipping retweet https://twitter.com/statuses/' + tweet.id_str);
-                        return;
+                        if (node.alwaysVerified === true && tweet.user.verified === true) {
+                            node.log('always include tweets from verified accounts https://twitter.com/statuses/' + tweet.id_str);
+                        } else {
+                            node.log('skipping retweet https://twitter.com/statuses/' + tweet.id_str);
+                            return;
+                        }
                     }
                     if (node.topicLanguage.indexOf(tweet.lang) < 0) {
                         node.log('skipping language https://twitter.com/statuses/' + tweet.id_str);
                         return;
                     }
-                    asyncLoadAndSend(node.loadImages, tweet, (tweet) => {
-                        node.send({payload: tweet});
-                    });
+                    if (node.tweetLimitCount < node.tweetLimit) {
+                        if (node.alwaysVerified === false || tweet.user.verified === false || node.userIDs.indexOf(tweet.user.id_str) < 0) {
+                            node.tweetLimitCount += 1;
+                            setTimeout(() => {
+                                node.tweetLimitCount -= 1;
+                            }, (60000 - ((Math.floor(Math.random() * 10) + 1) * 1000)));
+                        }
+                        asyncLoadAndSend(node.loadImages, tweet, (tweet) => {
+                            node.send({payload: tweet});
+                        });
+                    }
+                    else {
+                        node.log('skipping tweet due to tweet limit https://twitter.com/statuses/' + tweet.id_str);
+                        return;
+                    }
                 });
                 clearInterval(startInterval);
             }
